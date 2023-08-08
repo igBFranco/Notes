6146acbe6bf811bc0dfb52806a4dd2a8f15ecdc9
Igor Bueno Franco <88065589+igBFranco@users.noreply.github.com>
2023-08-07 22:55:28 -0300

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Igor Bueno Franco on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
